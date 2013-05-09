57af5171e8a38342b287c65c95dcfe9efae6d516
Connor Savage and James Pothen <connor+james@appacademy.io>
2013-05-09 09:52:38 -0400

    basic band pages complete

---
+++ app/controllers/bands_controller.rb
@@ -0,0 +1,23 @@
+class BandsController < ApplicationController
+  def index
+    @bands = Band.all
+  end
+
+  def new
+    @band = Band.new
+  end
+
+  def create
+    @band = Band.new(params[:band])
+
+    if @band.save
+      redirect_to bands_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @band = Band.find(params[:id])
+  end
+end
